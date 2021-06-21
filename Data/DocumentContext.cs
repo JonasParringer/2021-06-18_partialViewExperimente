1fcfcca4afb1d1567d7fa689e266737576f3cc1d
JonasParringer <jonasparringer@gmx.de>
2021-06-21 09:18:00 +0200

    So weit so gut

---
+++ Data/DocumentContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _2021_06_18_partialViewExperimente.Models;
+
+    public class DocumentContext : DbContext
+    {
+        public DocumentContext (DbContextOptions<DocumentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<_2021_06_18_partialViewExperimente.Models.Document> Document { get; set; }
+    }
---
+++ Models/Document.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2021_06_18_partialViewExperimente.Models
+{
+    public class Document
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
