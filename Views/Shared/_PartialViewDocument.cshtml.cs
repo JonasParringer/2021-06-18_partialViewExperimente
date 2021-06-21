30f752596806802c85e1c5a6ef6298673b7a159f
JonasParringer <jonasparringer@gmx.de>
2021-06-21 12:09:46 +0200

    Partial View funktioniert erst mal. Es lag an dem was in der View steht(automatisch generiert) und nicht an der View selbst

---
+++ Views/Shared/_PartialViewDocument.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using _2021_06_18_partialViewExperimente.Models;
+
+namespace _2021_06_18_partialViewExperimente.Views.Shared
+{
+    public class _PartialViewDocumentModel : PageModel
+    {
+        private readonly DocumentContext _context;
+
+        public _PartialViewDocumentModel(DocumentContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Document Document { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Document.Add(Document);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
